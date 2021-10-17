49eec19d78699e9e5ca5228225b45d914c7dd53f
6509CHEEMA <88336801+6509CHEEMA@users.noreply.github.com>
2021-10-17 17:18:33 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,78 +1,47 @@
-var p5Inst = new p5(null, 'sketch');
-
-window.preload = function () {
-  initMobileControls(p5Inst);
-
-  p5Inst._predefinedSpriteAnimations = {};
-  p5Inst._pauseSpriteAnimationsByDefault = false;
-  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
-  var orderedKeys = animationListJSON.orderedKeys;
-  var allAnimationsSingleFrame = false;
-  orderedKeys.forEach(function (key) {
-    var props = animationListJSON.propsByKey[key];
-    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
-    var image = loadImage(props.rootRelativePath, function () {
-      var spriteSheet = loadSpriteSheet(
-          image,
-          props.frameSize.x,
-          props.frameSize.y,
-          frameCount
-      );
-      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
-      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
-      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
-    });
-  });
-
-  function wrappedExportedCode(stage) {
-    if (stage === 'preload') {
-      if (setup !== window.setup) {
-        window.setup = setup;
-      } else {
-        return;
-      }
-    }
-// -----
-
+var paddle,ball;
+var edges=[];
+var box1,box2,box3,box4;
+function setup(){
 //creating plddle and the ball
-var paddle = createSprite(200, 375, 50, 15);
-var ball = createSprite(150, 250, 20, 20);
+paddle = createSprite(200, 375, 50, 15);
+ ball = createSprite(150, 250, 20, 20);
 
 //first row of boxes
-var box1 = createSprite(25, 75, 50, 50);
+ box1 = createSprite(25, 75, 50, 50);
 box1.shapeColor="red";
-var box2 = createSprite(75, 75, 50, 50);
+ box2 = createSprite(75, 75, 50, 50);
 box2.shapeColor="blue";
-var box3 = createSprite(125, 75, 50, 50);
+box3 = createSprite(125, 75, 50, 50);
 box3.shapeColor="red";
-var box4 = createSprite(175, 75, 50, 50);
+ box4 = createSprite(175, 75, 50, 50);
 box4.shapeColor="blue";
-var box5 = createSprite(225, 75, 50, 50);
+box5 = createSprite(225, 75, 50, 50);
 box5.shapeColor="red";
-var box6 = createSprite(275, 75, 50, 50);
+box6 = createSprite(275, 75, 50, 50);
 box6.shapeColor="blue";
-var box7 = createSprite(325, 75, 50, 50);
+ box7 = createSprite(325, 75, 50, 50);
 box7.shapeColor="red";
-var box8 = createSprite(375, 75, 50, 50);
+box8 = createSprite(375, 75, 50, 50);
 box8.shapeColor="blue";
 
 //second row of boxes
-var box9 = createSprite(25, 125, 50, 50);
+ box9 = createSprite(25, 125, 50, 50);
 box9.shapeColor="blue";
-var box10 = createSprite(75, 125, 50, 50);
+ box10 = createSprite(75, 125, 50, 50);
 box10.shapeColor="red";
-var box11 = createSprite(125, 125, 50, 50);
+box11 = createSprite(125, 125, 50, 50);
 box11.shapeColor="blue";
-var box12 = createSprite(175, 125, 50, 50);
+ box12 = createSprite(175, 125, 50, 50);
 box12.shapeColor="red";
-var box13 = createSprite(225, 125, 50, 50);
+box13 = createSprite(225, 125, 50, 50);
 box13.shapeColor="blue";
-var box14 = createSprite(275, 125, 50, 50);
+ box14 = createSprite(275, 125, 50, 50);
 box14.shapeColor="red";
-var box15 = createSprite(325, 125, 50, 50);
+ box15 = createSprite(325, 125, 50, 50);
 box15.shapeColor="blue";
-var box16 = createSprite(375, 125, 50, 50);
+ box16 = createSprite(375, 125, 50, 50);
 box16.shapeColor="red";
+}
  var score=0;
  var gamestate="serve";
 
@@ -92,13 +61,13 @@ function draw() {
     gamestate="play";
   }
   }
-  createEdgeSprites();
+  edges=createEdgeSprites();
   if (gamestate=="play"){
   //Moving the paddle with mouse along the x-axis
   paddle.x=World.mouseX;
   
   //destroy the boxes when ball touches them
-  if (ball.isTouching(bottomEdge)|| score==16){
+  if (ball.isTouching(edges[3])|| score==16){
     gamestate="end";
   }
   
@@ -106,9 +75,9 @@ function draw() {
   }
   //Making the ball bounceOff the paddle and three sides of canvas
   
-  ball.bounceOff(rightEdge);
-  ball.bounceOff(leftEdge);
-  ball.bounceOff(topEdge);
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[0]);
+  ball.bounceOff(edges[1]);
   ball.bounceOff(paddle);
   
   if (gamestate=="end"){
@@ -184,21 +153,3 @@ function draw() {
   drawSprites();
   }
 
-// -----
-    try { window.draw = draw; } catch (e) {}
-    switch (stage) {
-      case 'preload':
-        if (preload !== window.preload) { preload(); }
-        break;
-      case 'setup':
-        if (setup !== window.setup) { setup(); }
-        break;
-    }
-  }
-  window.wrappedExportedCode = wrappedExportedCode;
-  wrappedExportedCode('preload');
-};
-
-window.setup = function () {
-  window.wrappedExportedCode('setup');
-};
